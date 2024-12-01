da203b9d4f45db9504456263cfc680084e8689e2
MANISH <139268775+manishakh07@users.noreply.github.com>
2024-12-01 16:34:47 +0530

    third commit
    frontend data

---
+++ Frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import './FoodDisplay.css'
+import { StoreContext } from '../../context/storeContext'
+import FoodItem from '../FoodItem/FoodItem'
+import { food_list } from '../../assets/frontend_assets/assets'
+
+const FoodDisplay = ({ category }) => {
+
+  const { food_list } = useContext(StoreContext)
+  return (
+    <div className='food-display' id='food-display'>
+      <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          if(category==='All' || category === item.category){
+          return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} category={item.category} />
+          }
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default FoodDisplay
